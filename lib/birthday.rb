4bb62345fe7e785560394980948cc152229d93a0
Zach  Smith <zdsmith001@gmail.com>
2019-07-25 16:15:09 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,5 +16,6 @@ def age_appropriate_birthday(birthday_kids)
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
       puts "You are too old for this."
+    end
   end
 end
